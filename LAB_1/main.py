e3a664f70b4d990b850c755986859025427cec7c
root <root@andrey-deb>
2016-03-06 12:47:00 +0300

    Minor changes

--- LAB_1/main.py
+++ LAB_1/main.py
@@ -1,14 +1,34 @@
+import math.tanh as tanh
+
+
+def main_function(x1, x2, x3, x4):
+    if (x1+x2+x3)*(x2+x3+x4):
+        return 1
+    else:
+        return 0
+
+
+def fault_counter(t, y)
+    return t - y
+
+
 def net(x, w, w0=0):
     net = 0
     for i in range(0,5):
         net += w[i]*x[i]+w0
     return net
 
-def af_threshold:
-    pass
 
-def af_logistic:
-    pass
+def af_threshold(net):
+    if net >= 0:
+        return 1
+    else:
+        return 0
+
+
+def af_logistic(net):
+    return (tanh(net)+1)/2
+
 
 def read_file(file_path):
     file = open(file_path)
@@ -20,9 +40,11 @@ def read_file(file_path):
             input_data.append(float(line))
     return input_data
 
+
 def main():
     input_data = read_file("input.txt")
     print input_data
+
 
 if __name__ == "__main__":
     main()
