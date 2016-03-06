1e8cb7fa5b8a2270202c9e0ecfff1bb5efb14886
Andrey <dyatlikos3000@ya.ru>
2016-03-06 15:07:16 +0300

    Some improvments in log file

--- LAB_1/main.py
+++ LAB_1/main.py
@@ -21,8 +21,8 @@ def w_delta_threshold(x, n, b):
 
 
 
-def w_delta_logistic():
-    pass
+def w_delta_logistic(x, n, b, X, w, w0):
+    return x*n*b*af_logistic_derivative(net(X,w,w0))
 
 
 def net(x, w, w0):
@@ -65,16 +65,18 @@ def read_file(file_path):
     return input_data
 
 
-def write_log(file_path, F, Y, E, k):
+def write_log(file_path, F, Y, E, k, w, w0):
     file = open(file_path, 'a')
     file.write("k = " + str(k) + '\n')
+    file.write("w = " + str(w) + '\t')
+    file.write("w0 = " + str(w0) + '\n')
     file.write("F = " + str(F) + '\n')
     file.write("Y = " + str(Y) + '\n')
     file.write("E = " + str(E) + '\n')
     file.write("\n\n")
 
 
-def education(X, w, w0, x0, k):
+def education_threshold(X, w, w0, x0, k):
     F = list()
     Y = list()
     for i in range(0, len(X)):
@@ -88,7 +90,25 @@ def education(X, w, w0, x0, k):
             w[i] += w_delta_threshold(x[i], 0.3, b)
 
     E = fault_counter(F,Y)
-    write_log("output.txt", F, Y, E, k)
+    write_log("output.txt", F, Y, E, k, w, w0)
+    return E, w, w0
+
+
+def education_logistic(X, w, w0, x0, k):
+    F = list()
+    Y = list()
+    for i in range(0, len(X)):
+        F.append(main_function(X[i][0],X[i][1],X[i][2],X[i][3]))
+    for i in range(0,len(X)):
+        x = X[i]
+        Y.append(af_threshold(net(X[i], w, w0)))
+        b = F[i]-Y[i]
+        w0 += w_delta_logistic(x0, 0.3, b, x, w, w0)
+        for i in range (0, len(w)):
+            w[i] += w_delta_logistic(x[i], 0.3, b, x, w, w0)
+
+    E = fault_counter(F, Y)
+    write_log("output_1.txt", F, Y, E, k, w, w0)
     return E, w, w0
 
 
@@ -100,9 +120,17 @@ def main():
     k = 0
     X = read_file("input.txt")
     while E != 0:
-        E, w, w0 = education(X, w, w0, x0, k)
+        E, w, w0 = education_threshold(X, w, w0, x0, k)
         print E, w, w0
         k += 1
-
+    w = [0,0,0,0]
+    w0 = 0
+    x0 = 1
+    E = 1
+    k = 0
+    while E != 0:
+        E, w, w0 = education_logistic(X, w, w0, x0, k)
+        print E, w, w0
+        k += 1
 if __name__ == "__main__":
     main()
