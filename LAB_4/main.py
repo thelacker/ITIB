e53ded30080ca023b46448ac82ce18fd799c8723
thelacker <dyatlikos3000@ya.ru>
2016-04-27 17:58:24 +0300

    Done

--- LAB_4/main.py
+++ LAB_4/main.py
@@ -49,11 +49,14 @@ class Clasterisation:
         self.dots.append([event.x, event.y])
 
     def euclidean(self, event):
+        self.proceed('E')
+
+    def proceed(self, method):
         old_centers = list()
         old_clusters = list()
         for i in range(self.dimension):
             self.centers.append(self.dots[randint(0, int(len(self.dots)) - 1)])
-        self.classify()
+        self.classify(method=method)
         self.centers = list()
         old_centers.append(self.centers)
         old_clusters.append(self.clusters)
@@ -63,7 +66,7 @@ class Clasterisation:
             self.find_center()
             if old_centers.pop() == self.centers or old_clusters.pop() == self.clusters:
                 break
-            self.classify()
+            self.classify(method=method)
             old_centers.append(self.centers)
             old_clusters.append(self.clusters)
             self.centers = list()
@@ -87,17 +90,20 @@ class Clasterisation:
             y = [item[1] for item in self.clusters[i]]
             self.centers.append([median(x), median(y)])
 
-    def classify(self):
+    def classify(self, method):
         self.flush(False)
         values = list()
         for dot in self.dots:
             for i in range(len(self.centers)):
-                values.append(self.metric_euclidean(dot, self.centers[i]))
+                if method == 'E':
+                    values.append(self.metric_euclidean(dot, self.centers[i]))
+                if method == 'M':
+                    values.append(self.metric_manhattan(dot, self.centers[i]))
             self.clusters[values.index(min(values))].append(dot)
             values = list()
 
     def manhattan(self, event):
-        self.step = -1
+        self.proceed('M')
 
     def metric_euclidean(self, p1, p2):
         return sqrt(pow((p1[0] - p2[0]), 2) + pow((p1[1] - p2[1]), 2))
